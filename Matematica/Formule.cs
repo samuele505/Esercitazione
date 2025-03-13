0ff6a0631ce86ac43fe566d759f77422406e9c29
samuele505 <boccardellisamuele@gmail.com>
2025-03-13 12:55:46 +0100

    Ciao

---
+++ Matematica/Formule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercitazione1.Matematica
+{
+    public class Formule
+    {
+        public static double Somma(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
